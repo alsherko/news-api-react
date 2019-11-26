ae06e99031d9b633408ed33b82f7de0be62a4032
alsherko <al.sherko@gmail.com>
2019-11-26 15:54:18 +0300

    add media query of article for ipad width

--- src/components/articles/Article.js
+++ src/components/articles/Article.js
@@ -9,13 +9,17 @@ const Article = ({ article }) => {
       <h3 className="article__title">
         <a href={article.url}>{article.title}</a>
       </h3>
-      {(article.author || article.publishedAt) && (
+      {article.description && (
         <summary className="article__description">
           <p>{article.description}</p>
-          {article.author && (
-            <span className="article__author">{article.author}</span>
+          {(article.author || article.publishedAt) && (
+            <mark>
+              {article.author && (
+                <span className="article__author">{article.author}</span>
+              )}
+              {article.publishedAt && <time>{date.toLocaleString()}</time>}
+            </mark>
           )}
-          {article.publishedAt && <time>{date.toLocaleString()}</time>}
         </summary>
       )}
       {article.content && <p className="article__content">{article.content}</p>}
