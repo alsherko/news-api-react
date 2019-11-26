d7f58b8740e6c37b056acb41385bf5d4ac195e45
alsherko <al.sherko@gmail.com>
2019-11-26 14:17:22 +0300

    remove the extra code to get the current date

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,13 +3,12 @@ import Search from './Search'
 import './Header.css'
 
 const Header = () => {
-  const currentDate = new Date().toJSON()
-  const [yyyy, mm, dd] = currentDate.slice(0, 10).split('-')
+  const currentDate = new Date().toLocaleDateString()
   return (
     <header className="header">
       <h1 className="header__title">News App</h1>
       <Search />
-      <time dateTime={currentDate}>{`${dd}-${mm}-${yyyy}`}</time>
+      <time dateTime={currentDate}>{currentDate}</time>
     </header>
   )
 }
