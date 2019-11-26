8301e23eb3e8e99b18f62da07980696df9a015fa
alsherko <al.sherko@gmail.com>
2019-11-26 18:44:44 +0300

    delete unused constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,5 @@ const API = 'https://newsapi.org/v2/'
 const API_KEY = process.env.REACT_APP_API_KEY
 const PAGE_SIZE = 50
 const WAIT_INTERVAL = 1000
-const ENTER_KEY = 13
 
-export { API, API_KEY, PAGE_SIZE, WAIT_INTERVAL, ENTER_KEY }
+export { API, API_KEY, PAGE_SIZE, WAIT_INTERVAL }
