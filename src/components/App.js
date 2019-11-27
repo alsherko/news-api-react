e114b5e0d07a98c04762b6d2da33208dd7505a7b
alsherko <al.sherko@gmail.com>
2019-11-27 16:00:33 +0300

    add Footer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Header from './header/Header'
 import Toolbar from './Toolbar'
 import ArticleContainer from '../container/ArticleContainer'
 import SourceContainer from '../container/SourceContainer'
+import Footer from './Footer'
 import { ArticleProvider } from '../context/ArticleContext'
 import './App.css'
 
 const App = () => {
   return (
-    <ArticleProvider>
-      <Header />
-      <main className="main">
-        <Toolbar />
-        <ArticleContainer />
-        <SourceContainer />
-      </main>
-    </ArticleProvider>
+    <Fragment>
+      <ArticleProvider>
+        <Header />
+        <main className="main">
+          <Toolbar />
+          <ArticleContainer />
+          <SourceContainer />
+        </main>
+      </ArticleProvider>
+      <Footer />
+    </Fragment>
   )
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <a href="mailto:al.sherko@gmail.com">Contact us</a>
+    </footer>
+  )
+}
+
+export default Footer
