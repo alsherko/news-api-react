056e8bc0c5ddd477d12e050a008d06303255e762
alsherko <al.sherko@gmail.com>
2019-11-27 09:20:45 +0300

    update page size constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 const API = 'https://newsapi.org/v2/'
 const API_KEY = process.env.REACT_APP_API_KEY
-const PAGE_SIZE = 50
+const PAGE_SIZE = 10
 const WAIT_INTERVAL = 1000
 
 export { API, API_KEY, PAGE_SIZE, WAIT_INTERVAL }
