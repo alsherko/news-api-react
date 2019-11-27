41c47d70faf59df398a5059cd5bac6dcc29cf3d2
alsherko <al.sherko@gmail.com>
2019-11-27 10:12:15 +0300

    add loader and error handling to the SourceContainer

--- src/container/SourceContainer.js
+++ src/container/SourceContainer.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { Fragment, useState, useEffect, useContext } from 'react'
 import SourceList from '../components/sources/SourceList'
 import Pagination from '../components/Pagination'
+import Loader from '../components/Loader'
 import { ArticleContext } from '../context/ArticleContext'
 import { API, API_KEY } from '../constants'
 
@@ -8,18 +9,19 @@ const SOURCE_PAGE_SIZE = 8
 
 const SourceContainer = () => {
   const appContext = useContext(ArticleContext)
-  const { language, searchValue } = appContext
+  const { language, searchValue, isLoading, hasErrored } = appContext
   const [sources, setSources] = useState([])
   const [page, setPage] = useState(1)
   const [lastPage, setLastPage] = useState(1)
   const [searchResults, setSearchResults] = useState([])
 
-  const [isLoading, setIsLoading] = useState(false)
-  const [hasErrored, setHasErrored] = useState(false)
+  const [sourceIsLoading, setSourceIsLoading] = useState(false)
+  const [sourceHasErrored, setSourceHasErrored] = useState(false)
+  const [sourceErrorMessage, setSourceErrorMessage] = useState('')
 
   useEffect(() => {
     const fetchSources = async () => {
-      setIsLoading(true)
+      setSourceIsLoading(true)
       const url = new URL(`${API}sources`),
         params = {
           apiKey: API_KEY,
@@ -35,10 +37,11 @@ const SourceContainer = () => {
         setLastPage(Math.ceil(sources.length / SOURCE_PAGE_SIZE))
       } catch (e) {
         if (e) {
-          setHasErrored(true)
+          setSourceHasErrored(true)
+          setSourceErrorMessage(e.message)
         }
       } finally {
-        setIsLoading(false)
+        setSourceIsLoading(false)
       }
     }
 
@@ -64,36 +67,41 @@ const SourceContainer = () => {
     onSearchSources()
   }, [searchValue, sources])
 
-  if (hasErrored) {
-    return <p> Sorry an error occured while loading the sources </p>
-  }
-
-  if (isLoading) {
-  }
-
   const sourcesList =
     searchValue !== undefined && searchValue.length > 0
       ? searchResults
       : sources
+
   return (
     <aside>
-      <Pagination
-        className="pagination-centered"
-        page={page}
-        pageSize={SOURCE_PAGE_SIZE}
-        lastPage={lastPage}
-        setPage={setPage}
-      />
-      <SourceList
-        sources={
-          sourcesList !== undefined
-            ? sourcesList.slice(
-                (page - 1) * SOURCE_PAGE_SIZE,
-                (page - 1) * SOURCE_PAGE_SIZE + SOURCE_PAGE_SIZE
-              )
-            : []
-        }
-      />
+      {!isLoading && sourceIsLoading && <Loader />}
+      {!hasErrored && sourceHasErrored && (
+        <Fragment>
+          <p> Sorry an error occured while loading the sources. </p>
+          <p> {`${sourceErrorMessage}`} </p>
+        </Fragment>
+      )}
+      {!sourceIsLoading && !sourceHasErrored && (
+        <Fragment>
+          <Pagination
+            className="pagination-centered"
+            page={page}
+            pageSize={SOURCE_PAGE_SIZE}
+            lastPage={lastPage}
+            setPage={setPage}
+          />
+          <SourceList
+            sources={
+              sourcesList !== undefined
+                ? sourcesList.slice(
+                    (page - 1) * SOURCE_PAGE_SIZE,
+                    (page - 1) * SOURCE_PAGE_SIZE + SOURCE_PAGE_SIZE
+                  )
+                : []
+            }
+          />
+        </Fragment>
+      )}
     </aside>
   )
 }
