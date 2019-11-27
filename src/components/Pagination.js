915e800f8db41ccf3f457e9759d6945bd97bf2f8
alsherko <al.sherko@gmail.com>
2019-11-27 09:11:33 +0300

    add the ability to transfer className from outside, validation max and min page value and propsType

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './Pagination.css'
 
-const Pagination = ({ page, pageSize, lastPage, setPage }) => {
+const Pagination = ({ page, pageSize, lastPage, setPage, className }) => {
   return (
-    <div className="pagination">
+    <div className={`pagination ${className}`}>
       <span>{`${pageSize} per page`}</span>
       <input
         className="pagination__input"
@@ -11,25 +12,38 @@ const Pagination = ({ page, pageSize, lastPage, setPage }) => {
         min="1"
         max={lastPage}
         value={page}
-        onChange={event => setPage(parseInt(event.target.value))}
+        onChange={event => {
+          const value = event.target.value
+          setPage(value > lastPage ? lastPage : value < 1 ? 1 : value)
+        }}
       />
       <span>{`/ ${lastPage}`}</span>
       <span className="pagination__buttons">
         <button
           className="pagination__button-left"
           onClick={() => setPage(parseInt(page) - 1)}
+          disabled={page < 2}
         >
           &laquo;
         </button>
         <button
           className="pagination__button-right"
           onClick={() => setPage(parseInt(page) + 1)}
+          disabled={page > lastPage - 1}
         >
           &raquo;
         </button>
       </span>
     </div>
   )
+}
+
+Pagination.propTypes = {
+  page: PropTypes.number,
+  pageSize: PropTypes.number,
+  lastPage: PropTypes.number,
+  setPage: PropTypes.func,
+  className: PropTypes.string,
 }
 
 export default Pagination
