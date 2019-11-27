662b2b1c0ce9f9a39c1cedf6edd72eb4dc5ec9f8
alsherko <al.sherko@gmail.com>
2019-11-27 10:23:15 +0300

    arranged Header, Article and Source components inside App

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Header from './header/Header'
+import ArticleContainer from '../container/ArticleContainer'
+import SourceContainer from '../container/SourceContainer'
+import { ArticleProvider } from '../context/ArticleContext'
 import './App.css'
 
-function App() {
-  return <div className="App"></div>
+const App = () => {
+  return (
+    <ArticleProvider>
+      <Header />
+      <main className="main">
+        <ArticleContainer />
+        <SourceContainer />
+      </main>
+    </ArticleProvider>
+  )
 }
 
-export { App }
+export default App
