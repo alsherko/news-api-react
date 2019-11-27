a9402775d4b25ca572839a02c0437fc10fc3d57c
alsherko <al.sherko@gmail.com>
2019-11-27 10:10:53 +0300

    add loader and error handling to the ArticleContainer

--- src/container/ArticleContainer.js
+++ src/container/ArticleContainer.js
@@ -1,23 +1,44 @@
-import React, { useContext } from 'react'
+import React, { Fragment, useContext } from 'react'
 import ArticleList from '../components/articles/ArticleList'
 import Pagination from '../components/Pagination'
+import Loader from '../components/Loader'
 import { ArticleContext } from '../context/ArticleContext'
 import { PAGE_SIZE } from '../constants'
 
 const ArticleContainer = () => {
   const appContext = useContext(ArticleContext)
-  const { articles, page, lastPage, setPage } = appContext
+  const {
+    articles,
+    page,
+    lastPage,
+    setPage,
+    hasErrored,
+    isLoading,
+    errorMessage,
+  } = appContext
 
   return (
     <section>
-      <ArticleList articles={articles} />
-      <Pagination
-        className="pagination-centered"
-        page={page}
-        pageSize={PAGE_SIZE}
-        lastPage={lastPage}
-        setPage={setPage}
-      />
+      {isLoading && <Loader />}
+      {hasErrored && (
+        <Fragment>
+          <p> Sorry an error occured while loading the articles. </p>
+          <p> {`${errorMessage}`} </p>
+        </Fragment>
+      )}
+
+      {!isLoading && !hasErrored && (
+        <Fragment>
+          <ArticleList articles={articles} />
+          <Pagination
+            className="pagination-centered"
+            page={page}
+            pageSize={PAGE_SIZE}
+            lastPage={lastPage}
+            setPage={setPage}
+          />
+        </Fragment>
+      )}
     </section>
   )
 }
