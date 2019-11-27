de07eb643c819913d58bb0cc1f3f23e24178aad1
alsherko <al.sherko@gmail.com>
2019-11-27 14:47:01 +0300

    Updates

---
+++ precache-manifest.f6a942379d20edf93b037d921896ea47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8606a95234f95f6acc825679abeda048",
+    "url": "/news-api-react/index.html"
+  },
+  {
+    "revision": "9e1f5e606741b00f76c0",
+    "url": "/news-api-react/static/css/main.b6c636b4.chunk.css"
+  },
+  {
+    "revision": "0f8df699156fef9d7b4d",
+    "url": "/news-api-react/static/js/2.53e94ff9.chunk.js"
+  },
+  {
+    "revision": "9e1f5e606741b00f76c0",
+    "url": "/news-api-react/static/js/main.cb80a9a3.chunk.js"
+  },
+  {
+    "revision": "2b6d68bc79cbd03669ad",
+    "url": "/news-api-react/static/js/runtime-main.38f06ff2.js"
+  }
+]);
